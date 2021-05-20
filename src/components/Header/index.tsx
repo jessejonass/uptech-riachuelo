5984b281ad00797ce7a34b870fec88ba5bf6d933
Jesse Jonas <jessejonas13@gmail.com>
2021-05-20 14:05:09 -0300

    feat: quando clica na logo na header, zera a busca

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSearch } from '../../hooks/search';
 
 import './styles.scss';
 import logo from '../../assets/logo.svg';
 
 const Header: React.FC = () => {
+    const { searchBooks } = useSearch();
     return (
         <div className="header__container">
-            <Link to="/">
+            <Link to="/" onClick={() => searchBooks('')}>
                 <img src={logo} alt="Logo" />
             </Link>
             <strong>Books+</strong>
